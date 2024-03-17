402ae170ba5885238b6ea89fa3033e078e5f14f0
moazzamsadiq <moazzam@syntaxtechs.com>
2024-03-17 19:25:13 -0400

    fdsf

--- src/test/java/steps/SearchAndCartManagement.java
+++ src/test/java/steps/SearchAndCartManagement.java
@@ -3,7 +3,11 @@ package steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utils.CommonMethods;
+
+import java.util.List;
 
 public class SearchAndCartManagement extends CommonMethods {
 
@@ -18,7 +22,20 @@ public class SearchAndCartManagement extends CommonMethods {
     }
     @Then("they should see only items with {string} in the title")
     public void they_should_see_only_items_with_in_the_title(String searchButton) {
-        System.out.println("result");
+        String noOfPages = searchPage.lastPage.getText();
+        boolean end=false;
+        while(!end){
+            List<WebElement> allData = searchPage.searchResultTable;
+            for (WebElement data:allData){
+                System.out.println( data.getText());
+
+            }
+            if(!end){
+                click(searchPage.nextButton);
+            }
+
+        }
+
     }
 
 }
